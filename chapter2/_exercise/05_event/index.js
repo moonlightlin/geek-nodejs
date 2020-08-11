a2446c1675a5c67b4396017e6eaf101ce6bd5384
月霖 <moonlight_lin@126.com>
2020-08-11 16:01:44 +0800

    event

--- chapter2/_exercise/05_event/index.js
+++ chapter2/_exercise/05_event/index.js
@@ -1,13 +1,4 @@
-const EventEmitter = require('events').EventEmitter
-
-class GeekTime extends EventEmitter {
-  constructor() {
-    super()
-    setInterval(() => {
-      this.emit('newlesson', { price: Math.random() * 100 })
-    }, 3000)
-  }
-}
+const GeekTime = require('./GeekTime')
 
 const geektime = new GeekTime
 geektime.addListener('newlesson', (res) => {
